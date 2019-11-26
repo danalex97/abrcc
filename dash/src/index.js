becd6ef92eda3bb471d98682cf10b85a91a04a33
danalex97 <dan.alex97@yahoo.com>
2019-11-26 20:29:50 +0100

    [dash] Added basic dash player.

--- dash/src/index.js
+++ dash/src/index.js
@@ -1,6 +1,16 @@
-import readingTime from "reading-time";
+import { MediaPlayer } from 'dashjs';
+import readingTime from 'reading-time';
+
+function init() {
+    let url = "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd";
+    let player = MediaPlayer().create();
+    let video = document.querySelector('#videoPlayer');
+
+    player.initialize(video, url, true);
+}
 
 window.calcRT = ev => { 
     var stats = readingTime(ev.value).text;
     document.getElementById("readingTime").innerText = stats;
 };
+document.addEventListener("DOMContentLoaded", init);
