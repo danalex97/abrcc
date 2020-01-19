cb6e61e7c41eeb954d4bcd3ab24898f3b7fbb2f7
danalex97 <dan.alex97@yahoo.com>
2020-01-19 22:58:13 +0100

    [dash] Added festive.

---
+++ dash/src/algo/festive.js
@@ -0,0 +1,131 @@
+import { AbrAlgorithm } from '../algo/interface';
+import { ThrpPredictorGetter } from '../algo/getters';
+
+import { Decision, Value } from '../common/data';
+import { logging } from '../common/logger';
+
+
+const logger = logging('Festive');
+const diminuation_factor = 0.85;
+const alpha = 12;
+const horizon = 5;
+
+
+// [TODO] should come from manifest
+const bitrateArray = [300, 750, 1200, 1850, 2850, 4300];
+const switchUpThreshold = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const n = bitrateArray.length;
+
+
+export class Festive extends AbrAlgorithm {
+    constructor() {
+        super();
+
+        this.bandwidth = new ThrpPredictorGetter();
+        
+        this.qualityLog = { 0 : 0 };
+        this.prevQuality = 0;
+        this.lastIndex = 0;
+        this.switchUpCount = 0;
+    }
+
+    selectQuality(bitrate) {
+        let quality = n;
+        for (let i = n - 1; i >= 0; i--) {
+            quality = i;
+            if (bitrate >= bitrateArray[i]) {
+                break;
+            }
+        }
+        return quality;
+    }
+
+    getEfficiencyScore(b, b_ref, w) {
+        return Math.abs(bitrateArray[b] / Math.min(w, bitrateArray[b_ref]) - 1);
+    }
+
+    getStabilityScore(b, b_ref, b_cur) {
+        var score = 0,
+            changes = 0;
+        if (this.lastIndex >= 1) {
+            let start = Math.max(0, this.lastIndex + 1 - horizon);
+            let end = this.lastIndex - 1;
+            for (let i = start; i <= end; i++) {
+                if (this.qualityLog[i] != this.qualityLog[i + 1]) {
+                    changes++;
+                }
+            }
+        }
+        if (b != b_cur) {
+            changes += 1;
+        }
+        score = Math.pow(2, changes);
+        return score;
+    }
+
+    getCombinedScore(b, b_ref, b_cur, w) {
+        let stabilityScore  = this.getStabilityScore(b, b_ref, b_cur);
+        let efficiencyScore = this.getEfficiencyScore(b, b_ref, w);
+        return stabilityScore + alpha * efficiencyScore;
+    }
+
+    getDecision(metrics, index, timestamp) {
+        this.bandwidth.update(metrics, this.ctx);
+        let bwPrediction = this.bandwidth.value;
+
+        // compute b_target
+        let b_target = this.selectQuality(diminuation_factor * bwPrediction);
+        
+        // compute b_ref
+        let b_cur = this.prevQuality;
+        let b_ref = 0;
+        if (b_target > b_cur) {
+            this.switchUpCount = this.switchUpCount + 1;
+            if (this.switchUpCount > switchUpThreshold[b_cur]) {
+                b_ref = b_cur + 1;
+            } else {
+                b_ref = b_cur;
+            }
+        } else if (b_target < b_cur) {
+            b_ref = b_cur - 1;
+            this.switchUpCount = 0;
+        } else {
+            b_ref = b_cur;
+            this.switchUpCount = 0;
+        }
+        
+        // delayed update
+        let quality = 0;
+        if (b_ref != b_cur) {
+            // compute scores
+            let score_cur = this.getCombinedScore(b_cur, b_ref, b_cur, bwPrediction);
+            let score_ref = this.getCombinedScore(b_ref, b_ref, b_cur, bwPrediction);
+            logger.log(`score cur ${b_cur} -> ${score_cur}`, `score ref ${b_ref} -> ${score_ref}`);  
+
+            if (score_cur <= score_ref) {
+                quality = b_cur;
+            } else {
+                quality = b_ref;
+                if (quality > b_cur) {
+                    this.switchUpCount = 0;
+                }
+            }
+        } else {
+            quality = b_cur;
+        }
+
+        // log relevant info for festive
+        logger.log(`quality ${quality}`, `b_target ${b_target}`);
+
+        // update quality log
+        this.qualityLog[index] = bitrateArray[quality];
+        this.prevQuality = quality;
+        this.lastIndex = index;
+
+        return new Decision(
+            index,
+            quality,
+            timestamp,
+        );
+    }
+}
--- dash/src/algo/selector.js
+++ dash/src/algo/selector.js
@@ -1,5 +1,6 @@
 import { BB } from '../algo/bb';
 import { RB } from '../algo/rb';
+import { Festive } from '../algo/festive';
 
 
 export function GetAlgorithm(name) {
@@ -8,6 +9,9 @@ export function GetAlgorithm(name) {
     }
     if (name == 'rb') {
         return new RB();
+    }
+    if (name == 'festive') {
+        return new Festive();
     }
     return null;
 }
