2dac3b2f8f88e8bba047d94c7276b91b6ccbced4
danalex97 <dan.alex97@yahoo.com>
2020-05-01 16:22:49 +0200

    [quic] nit: Moved WorthedAbr in separate file.

--- quic/chromium/src/net/abrcc/abr/abr.h
+++ quic/chromium/src/net/abrcc/abr/abr.h
@@ -14,71 +14,12 @@
 #include "net/abrcc/structs/averages.h"
 
 #include "net/abrcc/abr/abr_base.h"
+#include "net/abrcc/abr/abr_worthed.h"
 
 #include <unordered_map>
 
+
 namespace quic {
-
-class StateTracker {
- public:
-  StateTracker(std::vector<int> bitrate_array);
-  virtual ~StateTracker();
-
-  void registerMetrics(const abr_schema::Metrics &);
- protected:
-  BbrAdapter::BbrInterface* interface; 
-
-  abr_schema::Value last_player_time;
-  abr_schema::Value last_buffer_level;
-  std::unique_ptr<structs::MovingAverage<double>> average_bandwidth;  
-
-  base::Optional<abr_schema::Value> last_bandwidth;
-  base::Optional<abr_schema::Value> last_rtt;
- private:
-  int last_timestamp;
-  std::vector<int> _bitrate_array;
-};
-
-class WorthedAbr : public SegmentProgressAbr, public StateTracker {
- public:
-  WorthedAbr(const std::shared_ptr<DashBackendConfig>& config);
-  ~WorthedAbr() override;
-
-  void registerMetrics(const abr_schema::Metrics &) override;
-  int decideQuality(int index) override;
- private:
-  // reward computation
-  double compute_reward(
-    std::vector<int> qualities, 
-    int start_index, 
-    int bandwidth,
-    int start_buffer,
-    int current_quality
-  );
-  std::pair<double, int> compute_reward_and_quality(
-    int start_index, 
-    int bandwidth,
-    int start_buffer,
-    int current_quality,
-    bool stochastic,
-    int last_decision
-  );
-
-  // aggresivity
-  double partial_bw_safe(double bw);
-  double factor(double bw, double delta);
-  double aggresivity(double bw, double delta);
-  
-  // utilities
-  int adjustedBufferLevel(int index);
-  
-  std::pair<int, int> computeRates(bool stochastic);
-  void adjustCC(); 
-  void setRttProbing(bool probing);
-
-  int ban;
-  bool is_rtt_probing;
-};
 
 class TargetAbr : public SegmentProgressAbr, public StateTracker {
  public:
