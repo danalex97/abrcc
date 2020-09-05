7d45cf4486af119467a84b54f9839de95b5c3790
danalex97 <dan.alex97@yahoo.com>
2020-09-05 14:02:56 +0300

    [doc] Added explainations for the RemoteAbr algorithm.

--- quic/chromium/src/net/abrcc/abr/abr_remote.h
+++ quic/chromium/src/net/abrcc/abr/abr_remote.h
@@ -19,6 +19,9 @@
 
 namespace quic {
 
+// ABR algorithm that externalizes the computation for target to a HTTP server.
+// The used congestion control backend can be either Target(`abrcc/cc/target`) 
+// or Gap(`abrcc/cc/gap`).
 class RemoteAbr : public TargetAbr2 {
  public:
   RemoteAbr(const std::shared_ptr<DashBackendConfig>& config);
@@ -27,6 +30,18 @@ class RemoteAbr : public TargetAbr2 {
   void registerMetrics(const abr_schema::Metrics &metrics) override;
   int decideQuality(int index) override;  
  private:
+  // Given the current state of the ABR:
+  //   - avg_bandwidth: average bandwidth computed using a Wilder Moving Average 
+  //   - currnet_bandwidth: the latest bandwidth as measured by BBR
+  //   - last_buffer: the buffer with the biggest timestamp 
+  //   - last_rtt: the latest RTT as measured by BBR
+  //   - current_quality: the quality for `current_index` - 1 
+  //   - current_index: the index to decide the quality for
+  //   - vmafs: the matrix of per-segment VMAFs for each quality band
+  //   - sizes: the matrix of per-segment sizes for each quality band
+  // Makes a request to the localhost at the fixed port RemosteAbrConstants::remote_port
+  // exposing the full ABR state and asking for a `target_bandwidth` value. The target 
+  // value will be passed to the CC backend. 
   int getTargetDecision(
     int avg_bandwidth,
     int current_bandwidth,
